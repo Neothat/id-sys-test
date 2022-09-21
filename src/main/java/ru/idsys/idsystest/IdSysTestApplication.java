d2262d46d8034008d7ad0652f23209de99f678a3
Denis <neothat@mail.ru>
2022-09-21 16:49:38 +0300

    Initial commit

---
+++ src/main/java/ru/idsys/idsystest/IdSysTestApplication.java
@@ -0,0 +1,13 @@
+package ru.idsys.idsystest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdSysTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdSysTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/idsys/idsystest/IdSysTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.idsys.idsystest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdSysTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
